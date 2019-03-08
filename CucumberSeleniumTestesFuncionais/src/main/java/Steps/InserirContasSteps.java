81fa23e19043adead358243acd32d8e461970716
739949 <739949@LTBR47492.cts.com>
2019-03-08 11:16:54 -0300

    Criação de conta, com teste negativo e gerenciamento de dados.

---
+++ CucumberSeleniumTestesFuncionais/src/main/java/Steps/InserirContasSteps.java
@@ -0,0 +1,99 @@
+package Steps;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.java.After;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
+
+public class InserirContasSteps {
+
+	WebDriver driver;
+
+	@Dado("^que estou acessando a aplicação$")
+	public void que_estou_acessando_a_aplicação() throws Throwable {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\739949\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("http://srbarriga.herokuapp.com/");
+	}
+
+	@Quando("^informo o usuário \"([^\"]*)\"$")
+	public void informo_o_usuário(String arg1) throws Throwable {
+		driver.findElement(By.id("email")).sendKeys(arg1);
+	}
+
+	@Quando("^a senha \"([^\"]*)\"$")
+	public void a_senha(String arg1) throws Throwable {
+		driver.findElement(By.id("senha")).sendKeys(arg1);
+	}
+
+	@Quando("^seleciono entrar$")
+	public void seleciono_entrar() throws Throwable {
+		driver.findElement(By.tagName("button")).click();
+	}
+
+	@Então("^visualizo a página inicial$")
+	public void visualizo_a_página_inicial() throws Throwable {
+		String texto = driver.findElement(By.xpath("/html/body/div[1]")).getText();
+		Assert.assertEquals("Bem vindo, Faby!", texto);
+		
+
+	}
+
+	@Quando("^seleciono Contas$")
+	public void seleciono_Contas() throws Throwable {
+		driver.findElement(By.linkText("Contas")).click();
+	}
+
+	@Quando("^seleciono Adicionar$")
+	public void seleciono_Adicionar() throws Throwable {
+		driver.findElement(By.linkText("Adicionar")).click();
+	}
+
+	@Quando("^informo a conta \"([^\"]*)\"$")
+	public void informo_a_conta(String arg1) throws Throwable {
+		driver.findElement(By.id("nome")).sendKeys(arg1);
+		
+	}
+		
+	@Quando("^seleciono Salvar$")
+	public void seleciono_Salvar() throws Throwable {
+		driver.findElement(By.tagName("button")).click();
+	}
+
+	@Então("^a conta é inserida com sucesso$")
+	public void a_conta_é_inserida_com_sucesso() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class='alert alert-success']")).getText();
+		Assert.assertEquals("Conta adicionada com sucesso!", texto);
+	}
+
+// segundo cenario
+	
+	@Então("^sou notificado que o nome da conta é obrigatório$")
+	public void souNotificadoQueONomeDaContaÉObrigatório() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
+		Assert.assertEquals("Informe o nome da conta", texto);
+	}
+	
+// terceiro cenario	
+
+	@Então("^sou notificado que já existe uma conta com esse nome$")
+	public void souNotificadoQueJáExisteUmaContaComEsseNome() throws Throwable {
+		String texto = driver.findElement(By.xpath("//div[@class=\"alert alert-danger\"]")).getText();
+		Assert.assertEquals("Já existe uma conta com esse nome!", texto);
+		
+	}
+			
+	@After
+	public void fecharBrowser() {
+		driver.quit();
+		
+	}
+	
+	
+}
---
+++ CucumberSeleniumTestesFuncionais/src/test/java/Runner/Runner.java
@@ -0,0 +1,39 @@
+package Runner;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.SnippetType;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+		features = "src/main/resources/Features/InserirConta.feature", glue = "Steps", tags = { "~@ignore" }, plugin = {
+				"pretty" }, monochrome = true, // serve para deixar o log mais harmoniozo
+//esse plugin irá juntar o nome do metodo sem usar underline
+		snippets = SnippetType.CAMELCASE, dryRun = false, // definido true para pegar somento o passo que falta
+		strict = false
+
+)
+
+public class Runner {
+
+	@BeforeClass
+	public static void reset() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\739949\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://srbarriga.herokuapp.com/");
+		driver.findElement(By.id("email")).sendKeys("fabiana@yahoo.com.br");
+		driver.findElement(By.id("senha")).sendKeys("cabeca");
+		driver.findElement(By.tagName("button")).click();
+		driver.findElement(By.linkText("reset")).click();
+		driver.quit();
+	}
+
+}
